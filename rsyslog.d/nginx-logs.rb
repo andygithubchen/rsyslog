ead01e23b5299f40ff892b3d29a2e18642699854
andychen <you@example.com>
2019-05-14 15:37:24 +0800

    fix

--- rsyslog.d/nginx-logs.rb
+++ rsyslog.d/nginx-logs.rb
@@ -1,5 +1,5 @@
 version=2
-rule=:%clientIp:word% %ident:word% %user:word% [%timestamp:char-to:]%] "%method:word% %request:word% HTTP/%http:float%" %status:number% %bytes:number% "%referrer:char-to:"%" "%agent:char-to:"%"%forwarded:rest%
+rule=:%clientIp:word% %ident:word% %user:word% [%timestamp:char-to:]%] "%host:char-to:"%" "%method:word% %request:word% HTTP/%http:float%" %status:number% %bytes:number% "%referrer:char-to:"%" "%agent:char-to:"%"%forwarded:rest%
 
 #https://github.com/rsyslog/rsyslog/issues/1734#issue-251578812
 
